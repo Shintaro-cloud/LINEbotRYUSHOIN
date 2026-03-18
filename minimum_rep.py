f060f3904e3d00e05a4b408e029fd10a97d7dc07
Shintaro-cloud <74037741+Shintaro-cloud@users.noreply.github.com>
2026-03-18 18:17:30 +0900

    Update minimum_rep.py

--- minimum_rep.py
+++ minimum_rep.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def reply_message(reply_token, text):
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
